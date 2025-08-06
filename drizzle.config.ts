21fd9dfb6e7c213e354846dbe631e5cc281fad41
Andcamilo <camilo.cb.97@gmail.com>
2025-08-06 14:45:01 -0500

    adding test project

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,13 +5,11 @@ export default {
   out: './drizzle',
   dialect: 'postgresql',
   dbCredentials: {
-    host: process.env.DB_HOST || '172.25.0.3',
+    host: process.env.DB_HOST || '100.66.76.2',
     port: parseInt(process.env.DB_PORT || '5432'),
     user: process.env.DB_USER || 'n8n_user',
     password: process.env.DB_PASSWORD || 'sg*?esXL}>z9wO4f',
     database: process.env.DB_NAME || 'n8n_db',
-    ssl: {
-      rejectUnauthorized: false,
-    },
+    ssl: false,
   },
 } satisfies Config;
--- src/db/index.ts
+++ src/db/index.ts
@@ -3,15 +3,13 @@ import postgres from 'postgres';
 import * as schema from './schema';
 
 const client = postgres({
-  host: process.env.DB_HOST || '172.25.0.3',
+  host: process.env.DB_HOST || '100.66.76.2',
   port: parseInt(process.env.DB_PORT || '5432'),
   database: process.env.DB_NAME || 'n8n_db',
   username: process.env.DB_USER || 'n8n_user',
   password: process.env.DB_PASSWORD || 'sg*?esXL}>z9wO4f',
   max: 1,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  ssl: false,
 });
 
 export const db = drizzle(client, { schema });
